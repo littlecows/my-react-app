ab6e5e2980dc38640f14c3f7865df1a0bc58bee9
littlecows <bfbz800@gmail.com>
2024-06-09 15:00:59 +0700

    hello hee

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,15 @@
 // import MyComponent from "./MyComponent";
 // import Counter from "./Counter";
 // import MyComponent1 from "./MyComponent1";
-import ColorPicker from "./ColorPicker";
+// import ColorPicker from "./ColorPicker";
+// import Mycomponent2 from "./MyComponent2.jsx";
+import MyComponent3 from "./MyComponent3.jsx";
 
 function App() {
 
   return (
     <>
-     <ColorPicker />
+     <MyComponent3 />
     </>
   );
 }
